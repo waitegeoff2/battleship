46a0d005dc5723a8031787c2bc17c4f429d26521
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-04-17 09:27:14 -0400

    Create gameboard object as an array

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,5 +1,32 @@
+import { Ship } from "./ship";
+
 class Gameboard {
     constructor() {
-        
+        this.rows = 10;
+        this.columns = 10;
+        this.board = [];
+    }
+
+    createBoard() {
+        for (let i = 0; i < this.rows; i++) {
+            this.board[i] = [];
+            for (let j = 0; j < this.columns; j++) {
+              this.board[i].push(Cell());
+              //push ([i, j])
+            }
+        }
+          
+    }
+
+    placeShip(){
+        //create a new ship
+        //pick starting poing
+        //pick direction
+        //see if possible
     }
 }
+
+let testBoard = new Gameboard;
+console.log(testBoard.createBoard());
+
+export {Gameboard}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import "./styles.css";
 import { Ship } from "./ship";
+import { Gameboard } from "./gameboard";
