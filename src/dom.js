a4f52cf49c2085b51f70819d0c23baf927b5eef5
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-04-18 13:13:12 -0400

    Added event listener for each cell on board

--- src/dom.js
+++ src/dom.js
@@ -8,17 +8,26 @@ function displayBoard(player) {
     let gameboard = player.gameboard.board;
 
     for(let i=0; i<gameboard.length; i++) {
-        
+        //create a new row for each array
         const row = document.createElement("div");
         row.classList.add("game-row"); 
-       
         for(let j=0; j<gameboard[i].length; j++) {
+            //append each column to each row
             const column = document.createElement("div");
             column.classList.add("game-column");
             row.appendChild(column);
+
+            //record a coordinate here to click later when choosing boats 
+            let coord = gameboard[i][j]
+            //add LISTENERS here for mouseover****
+            column.addEventListener('click', () => {
+                column.style.backgroundColor = "grey";
+            });
         }
+        //append the entire row and loop back to build the next row
         boardDisplay.appendChild(row);
     } 
+    return gameboard;
 }
 
 displayBoard(testPlayer);
