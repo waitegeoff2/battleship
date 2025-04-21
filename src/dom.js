fe3ab3806a2ffafdee521e9438ea33af217103a6
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-04-21 09:39:51 -0400

    Added a separate board for computer

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,11 @@
 import { Gameboard } from "./gameboard";
 import { Player } from "./player";
-const boardDisplay = document.querySelector(".game-board")
+const boardHumanDisplay = document.querySelector(".player-board")
+const boardCompDisplay = document.querySelector(".computer-board")
 
 let testPlayer = new Player("g");
 
-function displayBoard(player) {
+function displayHumanBoard(player) {
     let gameboard = player.gameboard.board;
 
     for(let i=0; i<gameboard.length; i++) {
@@ -25,11 +26,51 @@ function displayBoard(player) {
             });
         }
         //append the entire row and loop back to build the next row
-        boardDisplay.appendChild(row);
+        boardHumanDisplay.appendChild(row);
     } 
     return gameboard;
 }
 
-displayBoard(testPlayer);
+function displayComputerBoard(player) {
+    let gameboard = player.gameboard.board;
 
-export {displayBoard}
+    for(let i=0; i<gameboard.length; i++) {
+        //create a new row for each array
+        const row = document.createElement("div");
+        row.classList.add("game-row"); 
+        for(let j=0; j<gameboard[i].length; j++) {
+            //append each column to each row
+            const column = document.createElement("div");
+            column.classList.add("game-column");
+            row.appendChild(column);
+
+            //record a coordinate here to click later when choosing boats 
+            let coord = gameboard[i][j]
+            //add LISTENERS here for mouseover****
+            column.addEventListener('click', () => {
+                column.style.backgroundColor = "grey";
+            });
+        }
+        //append the entire row and loop back to build the next row
+        boardCompDisplay.appendChild(row);
+    } 
+    return gameboard;
+}
+
+//EVENT LISTENERS TO PLACE SHIPS
+//FIGURE OUT THE PAGE LAYOUT FIRST
+//button that says "place destroyer" fills out the addShip(row, column, length, direction) function
+
+function humanPlaceShip() {
+    //prompts to fill out the addShip(row, column, length, direction) function
+}
+
+function computerPlaceShip() {
+
+}
+
+function gameFlow() {
+
+}
+
+export {displayHumanBoard, displayComputerBoard}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import "./styles.css";
 import { Ship } from "./ship";
 import { Gameboard } from "./gameboard";
 import { Player } from "./player";
-import {displayBoard} from "./dom";
+import {displayHumanBoard, displayComputerBoard} from "./dom";
 
 let playerOne = new Player("Player")
 playerOne.human = true
 
 let playerComp = new Player("Computer")
 playerComp.human = false
+
+displayHumanBoard(playerOne);
+displayComputerBoard(playerComp);
 
 
 
